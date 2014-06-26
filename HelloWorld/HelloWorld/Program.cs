355c14de3d351412f1fd613e8ae15f5b2f56f00d
Majidah <majidahb@hotmail.com>
2014-06-26 11:03:09 -0400

    Create Application
    Application

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputReadLine;
+
+            Console.WriteLine("Hellow Word");
+            inputReadLine = Console.ReadLine();
+            Console.Write(inputReadLine);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
